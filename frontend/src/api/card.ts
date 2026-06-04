fed447c462477485d5931d08289133b573d36b3c
Jaden Lu <jadenlu@Andy.local>
2026-06-04 18:02:17 -0400

    Complete api for Card, Deck Config, Deck and Practice Session

---
+++ frontend/src/api/card.ts
@@ -0,0 +1,47 @@
+import { client, displayError } from "./client";
+import type { components } from "src/api/types";
+
+export const createCard = async (
+  payload: components["schemas"]["CardCreate"],
+) => {
+  const { data, error } = await client.POST("/api/cards/card", {
+    body: payload,
+  });
+  if (error) {
+    displayError(error);
+  }
+  return data;
+};
+
+export const readCard = async (cardId: string) => {
+  const { data, error } = await client.GET("/api/cards/card/{card_id}", {
+    params: { path: { card_id: cardId } },
+  });
+  if (error) {
+    displayError(error);
+  }
+  return data;
+};
+
+export const updateCard = async (
+  cardId: string,
+  payload: components["schemas"]["CardUpdate"],
+) => {
+  const { data, error } = await client.PUT("/api/cards/card/{card_id}", {
+    params: { path: { card_id: cardId } },
+    body: payload,
+  });
+  if (error) {
+    displayError(error);
+  }
+  return data;
+};
+
+export const deleteCard = async (cardId: string) => {
+  const { error } = await client.DELETE("/api/cards/card/{card_id}", {
+    params: { path: { card_id: cardId } },
+  });
+  if (error) {
+    displayError(error);
+  }
+};
---
+++ frontend/src/api/practice_session.ts
@@ -0,0 +1,49 @@
+import { client, displayError } from "./client";
+import type { components } from "src/api/types";
+
+export const createPracticeSession = async (
+  payload: components["schemas"]["PracticeSessionCreate"],
+) => {
+  const { data, error } = await client.POST(
+    "/api/practice_sessions/practice_session",
+    {
+      body: payload,
+    },
+  );
+  if (error) {
+    displayError(error);
+  }
+  return data;
+};
+
+export const readPracticeSession = async (practiceSessionId: string) => {
+  const { data, error } = await client.GET(
+    "/api/practice_sessions/practice_session/{practice_session_id}",
+    {
+      params: { path: { practice_session_id: practiceSessionId } },
+    },
+  );
+  if (error) {
+    displayError(error);
+  }
+  return data;
+};
+
+export const readPracticeCard = async (
+  practiceSessionId: string,
+  forward: boolean,
+) => {
+  const { data, error } = await client.GET(
+    "/api/practice_sessions/practice_session/{practice_session_id}/practice_card",
+    {
+      params: {
+        path: { practice_session_id: practiceSessionId },
+        query: { forward: forward },
+      },
+    },
+  );
+  if (error) {
+    displayError(error);
+  }
+  return data;
+};
